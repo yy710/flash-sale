96c0d906a28ea6f8bd80f46ea9ab78ce2b9d451d
yy710 <171254868@qq.com>
2018-03-12 17:39:38 +0800

    started...

---
+++ countdownTimer.js
@@ -0,0 +1,68 @@
+class CountdownTimer {
+  constructor() {
+    this.timers = [];
+  }
+
+  add(name, endTime = new Date(), callback = () => { }) {
+    this.timers.push({
+      name: name,
+      endtime: endTime,
+      total_micro_second: 0,
+      callback: callback
+    });
+  }
+
+  start(that) {
+    // 渲染倒计时时钟
+    this.timers.forEach(item=>{
+      item.total_micro_second = item.endTime - (new Date());
+      if (item.total_micro_second <= 0) {
+        
+        return;
+      }
+    });
+    
+    that.setData({ timers: date_format(total_micro_second) });
+
+    // timeout则跳出递归
+    if (total_micro_second <= 0) {
+      that.setData({
+        timer: "已经截止"
+      });
+      return;
+    }
+
+    // 放在最后--尾递归
+    setTimeout(() => {
+      total_micro_second -= 10;
+      this.start(that);
+    }
+      , 10)
+  }
+}
+
+// 时间格式化输出，如03:25:19 86。每10ms都会调用一次
+function date_format(micro_second) {
+  //计算剩余的天数
+  var days = Math.floor(micro_second / (1000 * 60 * 60 * 24));
+  // 秒数
+  micro_second -= days * (1000 * 60 * 60 * 24);
+  var second = Math.floor(micro_second / 1000);
+
+  // 小时位
+  var hr = Math.floor(second / 3600);
+  // 分钟位
+  var min = fill_zero_prefix(Math.floor((second - hr * 3600) / 60));
+  // 秒位
+  var sec = fill_zero_prefix((second - hr * 3600 - min * 60));// equal to => var sec = second % 60;
+  // 毫秒位，保留2位
+  var micro_sec = fill_zero_prefix(Math.floor((micro_second % 1000) / 10));
+  return `剩${days}天${hr}小时${min}分${sec}.${micro_sec}秒`;
+}
+
+// 位数不足补零
+function fill_zero_prefix(num) {
+  return num < 10 ? "0" + num : num
+}
+
+module.exports = CountdownTimer;
